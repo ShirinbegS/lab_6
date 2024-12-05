4457fee1adb43e001fa1e2b7a19a90f3b9b67179
root <root@LAPTOP-SG2OVKS4>
2024-12-05 15:20:42 +0500

    good

--- 3.py
+++ 3.py
@@ -1,22 +1,12 @@
 # Функция для проверки, является ли число простым
 def f(x):
-    if x < 2:
-        return False
-    if x==2:
-        return True
-    if x%2==0:
-        return False
-    for i in range(2, int(x**0.5)+1, 2):
+    for i in range(2, x//2+1):
         if x % i == 0:
             return False
     return True
-# Диапазон чисел
-start = 245690
-end = 245756
-
 # Нахождение и вывод простых чисел с их порядковыми номерами
 count = 0
-for num in range(start, end + 1):
+for num in range(245690, 245756 + 1):
     if f(num):
         count += 1
         print(count, num)
